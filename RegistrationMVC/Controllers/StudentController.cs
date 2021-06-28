2d83aa46a0197c7e06e59f309b5e1ed17aebe22e
aaryashaji1 <aaryashajit@gmail.com>
2021-06-28 18:13:34 -0500

    Add project files.

---
+++ RegistrationMVC/Controllers/StudentController.cs
@@ -0,0 +1,70 @@
+﻿using RegistrationMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistrationMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        string connectionString = "Server=ARYASHAJI\\SQLEXPRESS;Database=RegistrationMVC;Trusted_Connection=True";
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index1(Student student)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("StudentSave", connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("Name", student.Name);
+            command.Parameters.AddWithValue("Address", student.Address);
+            command.Parameters.AddWithValue("Gender", student.Gender);
+            command.Parameters.AddWithValue("Dob", student.Dob);
+            command.Parameters.AddWithValue("Pincode", student.Pincode);
+            command.Parameters.AddWithValue("Username",student.Username);
+            command.Parameters.AddWithValue("Password", student.Password);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+            return View("About", student);
+
+        }
+        public ActionResult Index2(string username, string password)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("StudentRead", connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("Username", username);
+            command.Parameters.AddWithValue("Password", password);
+
+            SqlDataReader reader = command.ExecuteReader();
+            reader.Read();
+
+            Student student = new Student();
+
+            student.Name = reader["Name"].ToString();
+            student.Address = reader["Address"].ToString();
+
+            student.Gender = reader["Gender"].ToString();
+            student.Dob = Convert.ToDateTime(reader["Dob"]);
+            student.Pincode = Convert.ToInt32(reader["Pincode"]);
+
+            reader.Close();
+            connection.Close();
+
+            return View("Result", student);
+
+        }
+    }
+}
+          
+    
---
+++ RegistrationMVC/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationMVC.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+        public string Male { get; set; }
+        public string Female { get; set; }
+        public DateTime Dob { get; set; }
+        public int Pincode{ get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
